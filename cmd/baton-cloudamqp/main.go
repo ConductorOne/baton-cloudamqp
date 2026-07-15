b8ca85852bab0892a986752ddb27c177635e6660
Luisina Santos <luisina.santos@conductorone.com>
2026-07-15 15:59:06 -0300

    CXH-2050: containerize connector
    Migrate to SDK V2 interfaces (RunConnector, ResourceSyncerV2,
    ResourceProvisionerV2, ResourceDeleterV2), update baton-sdk to v0.18.4,
    and add connector display name/icon/help URL config metadata required
    for container deployment.

--- cmd/baton-cloudamqp/main.go
+++ cmd/baton-cloudamqp/main.go
@@ -2,15 +2,13 @@ package main
 
 import (
 	"context"
-	"fmt"
-	"os"
 
 	cfg "github.com/conductorone/baton-cloudamqp/pkg/config"
 	"github.com/conductorone/baton-cloudamqp/pkg/connector"
+	"github.com/conductorone/baton-sdk/pkg/cli"
 	"github.com/conductorone/baton-sdk/pkg/config"
 	"github.com/conductorone/baton-sdk/pkg/connectorbuilder"
 	"github.com/conductorone/baton-sdk/pkg/connectorrunner"
-	"github.com/conductorone/baton-sdk/pkg/types"
 	"github.com/grpc-ecosystem/go-grpc-middleware/logging/zap/ctxzap"
 	"go.uber.org/zap"
 )
@@ -20,41 +18,24 @@ var version = "dev"
 func main() {
 	ctx := context.Background()
 
-	_, cmd, err := config.DefineConfiguration(
+	config.RunConnector(
 		ctx,
 		"baton-cloudamqp",
-		getConnector,
+		version,
 		cfg.Config,
-		connectorrunner.WithDefaultCapabilitiesConnectorBuilder(&connector.CloudAMQP{}),
+		getConnector,
+		connectorrunner.WithDefaultCapabilitiesConnectorBuilderV2(&connector.CloudAMQP{}),
 	)
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err.Error())
-		os.Exit(1)
-	}
-
-	cmd.Version = version
-
-	err = cmd.Execute()
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err.Error())
-		os.Exit(1)
-	}
 }
 
-func getConnector(ctx context.Context, c *cfg.Cloudamqp) (types.ConnectorServer, error) {
+func getConnector(ctx context.Context, c *cfg.Cloudamqp, _ *cli.ConnectorOpts) (connectorbuilder.ConnectorBuilderV2, []connectorbuilder.Opt, error) {
 	l := ctxzap.Extract(ctx)
 
 	cloudamqpConnector, err := connector.New(ctx, c.Token, c.BaseUrl)
 	if err != nil {
 		l.Error("error creating connector", zap.Error(err))
-		return nil, err
+		return nil, nil, err
 	}
 
-	conn, err := connectorbuilder.NewConnector(ctx, cloudamqpConnector)
-	if err != nil {
-		l.Error("error creating connector", zap.Error(err))
-		return nil, err
-	}
-
-	return conn, nil
+	return cloudamqpConnector, nil, nil
 }
